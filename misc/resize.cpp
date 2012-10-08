bdfa60fcde6d38679ecaffe081f000e5ec3347f2
kyubuns <mail@kyubuns.net>
2012-10-08 19:18:28 +0900

    resize用

--- misc/resize.cpp
+++ misc/resize.cpp
@@ -2,18 +2,18 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-int
-main(int argc, char *argv[])
-{
+cv::Mat resize(cv::Mat &src_img, double a) {
+  cv::Mat dst_img(src_img.rows*a, src_img.cols*a, src_img.type());
+  cv::resize(src_img, dst_img, dst_img.size(), cv::INTER_LANCZOS4);
+  return dst_img;
+}
+
+/*
+int main(int argc, char *argv[]) {
 	cv::Mat src_img = cv::imread(argv[1]);
 	if(!src_img.data)return -1;
 
-  if(src_img.empty()) return -1; 
-  cv::Mat dst_img1(src_img.rows*10.0, src_img.cols*10.0, src_img.type());
-  cv::resize(src_img, dst_img1, dst_img1.size(), cv::INTER_LANCZOS4);
-
-  cv::namedWindow("resize image1", CV_WINDOW_AUTOSIZE|CV_WINDOW_FREERATIO);
-  cv::imwrite("kyuri.png", dst_img1);
-  cv::imshow("resize image1", dst_img1);
+  cv::imshow("resize image1", resize(src_img, 0.1));
   cv::waitKey(0);
 }
+*/
