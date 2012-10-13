bccc25818327be05786f03e8c6b53c837742684d
kyubuns <mail@kyubuns.net>
2012-10-14 08:28:13 +0900

    初期ファイル自動捜索

--- css/css.cpp
+++ css/css.cpp
@@ -36,11 +36,11 @@ int aaaa[3] = {0};
 time_t start_time;
 
 //数えるだけ
-void calc(const int s_, const int m_, const int l_) {
+void calc(const int s_, const int m_, const int l_, bool force = false) {
   static int bs = -1;
   static int bm = -1;
   static int bl = -1;
-  if(bs==s_&&bm==m_&&bl==l_) return;
+  if(!force&&bs==s_&&bm==m_&&bl==l_) return;
   std::cout << "---result---" << std::endl;
   std::cout << "large:  " << l_ << std::endl;
   std::cout << "medium: " << m_ << std::endl;
@@ -49,6 +49,45 @@ void calc(const int s_, const int m_, const int l_) {
   bm = m_;
   bl = l_;
   std::cout << std::endl;
+}
+
+void draw() {
+  //con
+  if(con_img.data == NULL) return;
+  cv::Mat copy = con_img.clone();
+  for(auto &tmp : point) {
+    cv::Point p = tmp.first;
+    int k = tmp.second;
+    cv::Scalar color(0,0,0);
+    if(k==small) color = cv::Scalar(255,0,0);
+    if(k==medium) color = cv::Scalar(0,255,0);
+    if(k==large) color = cv::Scalar(0,0,255);
+    if(k!=weapon) continue;
+    cv::circle(copy, p, 6, color, 2);
+  }
+  imshow("con", copy);
+  calc(aaaa[small], aaaa[medium], aaaa[large]);
+
+  //status
+  cv::Mat status = cv::Mat::zeros(cv::Size(200, 100), CV_8UC3);
+  cv::rectangle(status, cv::Point(0,0), cv::Point(200,100), cv::Scalar(255, 255, 255), -1);
+  std::string w;
+  if(weapon==small) w = "small";
+  if(weapon==medium) w = "medium";
+  if(weapon==large) w = "large";
+  cv::Scalar color(0,0,0);
+  if(weapon==small) color = cv::Scalar(255,0,0);
+  if(weapon==medium) color = cv::Scalar(0,255,0);
+  if(weapon==large) color = cv::Scalar(0,0,255);
+  cv::putText(status, w.c_str(), cv::Point(10, 30), cv::FONT_HERSHEY_SIMPLEX, 0.8, color, 2, CV_AA);
+
+  cv::putText(status, "time: ", cv::Point(10, 80), cv::FONT_HERSHEY_SIMPLEX, 0.4, cv::Scalar(0,0,0), 1, CV_AA);
+  time_t end_time;
+  std::time(&end_time);
+  time_t sec_time = std::difftime(end_time, start_time);
+  std::string mes = itos(sec_time);
+  cv::putText(status, mes.c_str(), cv::Point(50, 80), cv::FONT_HERSHEY_SIMPLEX, 0.8, cv::Scalar(0,0,0), 1, CV_AA);
+  imshow("status", status);
 }
 
 void hoge() {
@@ -142,45 +181,7 @@ void hoge() {
   }
 
   cout << endl;        cout << endl;        cout << endl;
-}
-
-void draw() {
-  //con
-  if(con_img.data == NULL) return;
-  cv::Mat copy = con_img.clone();
-  for(auto &tmp : point) {
-    cv::Point p = tmp.first;
-    int k = tmp.second;
-    cv::Scalar color(0,0,0);
-    if(k==small) color = cv::Scalar(255,0,0);
-    if(k==medium) color = cv::Scalar(0,255,0);
-    if(k==large) color = cv::Scalar(0,0,255);
-    if(k!=weapon) continue;
-    cv::circle(copy, p, 6, color, 2);
-  }
-  imshow("con", copy);
-  calc(aaaa[small], aaaa[medium], aaaa[large]);
-
-  //status
-  cv::Mat status = cv::Mat::zeros(cv::Size(200, 100), CV_8UC3);
-  cv::rectangle(status, cv::Point(0,0), cv::Point(200,100), cv::Scalar(255, 255, 255), -1);
-  std::string w;
-  if(weapon==small) w = "small";
-  if(weapon==medium) w = "medium";
-  if(weapon==large) w = "large";
-  cv::Scalar color(0,0,0);
-  if(weapon==small) color = cv::Scalar(255,0,0);
-  if(weapon==medium) color = cv::Scalar(0,255,0);
-  if(weapon==large) color = cv::Scalar(0,0,255);
-  cv::putText(status, w.c_str(), cv::Point(10, 30), cv::FONT_HERSHEY_SIMPLEX, 0.8, color, 2, CV_AA);
-
-  cv::putText(status, "time: ", cv::Point(10, 80), cv::FONT_HERSHEY_SIMPLEX, 0.4, cv::Scalar(0,0,0), 1, CV_AA);
-  time_t end_time;
-  std::time(&end_time);
-  time_t sec_time = std::difftime(end_time, start_time);
-  std::string mes = itos(sec_time);
-  cv::putText(status, mes.c_str(), cv::Point(50, 80), cv::FONT_HERSHEY_SIMPLEX, 0.8, cv::Scalar(0,0,0), 1, CV_AA);
-  imshow("status", status);
+  calc(aaaa[small], aaaa[medium], aaaa[large], true);
 }
 
 void con_mouse_callback(int event, int x, int y, int flag ,void*) {
@@ -242,12 +243,16 @@ bool load_image(const std::string &path) {
 }
 
 int main(int argc, char *argv[]) {
-  if(argc!=3) {
-    std::cout << "./css [loaddir] [no]" << std::endl;
+  if(argc!=2) {
+    std::cout << "./css [loaddir]" << std::endl;
     return 0;
   }
-  image_index = atoi(argv[2]);
-  if(load_image(argv[1]) == false) return -1;
+  image_index = 0;
+  for(int i=0;i<1000;++i) {
+    image_index++;
+    if(image_index > 999) image_index = 0;
+    if(load_image(argv[1]) == true) break;
+  }
   //setMouseCallback("source", src_mouse_callback, 0);
   std::time(&start_time);
 
@@ -284,9 +289,7 @@ int main(int argc, char *argv[]) {
     if(key == '1') { weapon = large; }
     if(key == '2') { weapon = medium; }
     if(key == '3') { weapon = small; }
-    if(key == 13) {
-      hoge();
-    }
+    if(key == 13) { hoge(); }
     draw();
   }
 
